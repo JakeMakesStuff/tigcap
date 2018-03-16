7ee7a4698a8dffe8fd886add7ee2fe2ba7636721
Mary <Ipadlover8322@gmail.com>
2018-03-16 15:54:38 -0400

    Add kuvien support (Closes #7) (#8)
    * Prettify and add kuvien
    
    * Fix tigcap not setting kuvien api key header
    
    * Fix kuvien uploader

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
-let capture = require("interactive-screenshot").capture
-const {dialog, autoUpdater, app, Menu, Tray, globalShortcut, clipboard, BrowserWindow, Notification, ipcMain} = require("electron")
-let os = require("os")
-let snekfetch = require("snekfetch")
+let capture = require("interactive-screenshot").capture;
+const {
+  dialog,
+  autoUpdater,
+  app,
+  Menu,
+  Tray,
+  globalShortcut,
+  clipboard,
+  BrowserWindow,
+  Notification,
+  ipcMain
+} = require("electron");
+let os = require("os");
+let snekfetch = require("snekfetch");
 
-
-let server = "https://hazel-fyhwhtssmo.now.sh/"
-let feed = `${server}/update/${process.platform}/${app.getVersion()}`
+let server = "https://hazel-fyhwhtssmo.now.sh/";
+let feed = `${server}/update/${process.platform}/${app.getVersion()}`;
 // autoUpdater.setFeedURL(feed)
 
 setInterval(function() {
-  autoUpdater.checkForUpdates()
-}, 60000)
+  autoUpdater.checkForUpdates();
+}, 60000);
 
 autoUpdater.on("error", function(message) {
-  console.error('There was a problem updating the application')
-  console.error(message)
-})
+  console.error("There was a problem updating the application");
+  console.error(message);
+});
 
 autoUpdater.on("update-downloaded", function(event, releaseNotes, releaseName) {
   const dialogOpts = {
@@ -23,191 +33,222 @@ autoUpdater.on("update-downloaded", function(event, releaseNotes, releaseName) {
     buttons: ["Restart", "Later"],
     title: "Application Update",
     message: process.platform == "win32" ? releaseNotes : releaseName,
-    detail: "A new version has been downloaded. Restart the application to apply the updates."
-  }
+    detail:
+      "A new version has been downloaded. Restart the application to apply the updates."
+  };
 
   dialog.showMessageBox(dialogOpts, function(response) {
-    if(response == 0) autoUpdater.quitAndInstall()
-  })
-})
+    if (response == 0) autoUpdater.quitAndInstall();
+  });
+});
 
 let defaultConfig = {
-  "img_host": "tig",
-  "srht_url": "https://srht.example.org",
-  "pomf_host": ["https://pomf.example.org/", "https://pomf-vanity.example.org/"],
-  "owoToken": "",
-  "owoUrl": "https://owo.whats-th.is/",
-  "shortcut": "CommandOrControl+Shift+C",
-  "nothingdomains_key": "",
-  "nothingdomains_vanity": ""
-}
+  img_host: "elixire",
+  srht_url: "https://srht.example.org",
+  pomf_host: ["https://pomf.example.org/", "https://pomf-vanity.example.org/"],
+  owoToken: "",
+  owoUrl: "https://owo.whats-th.is/",
+  shortcut: "CommandOrControl+Shift+C",
+  nothingdomains_key: "",
+  nothingdomains_vanity: "",
+  kuvien_key: ""
+};
 
-let config
+let config;
 
 try {
-  config = require(`${app.getPath("userData")}/config.json`)
-} catch(e) {
-  config = defaultConfig
+  config = require(`${app.getPath("userData")}/config.json`);
+} catch (e) {
+  config = defaultConfig;
 }
 
-if(app.dock) app.dock.hide()
+if (app.dock) app.dock.hide();
 
 ipcMain.on("reload", function(event, cfg) {
-  console.log("Reloading...")
-  config = JSON.parse(cfg)
-  globalShortcut.unregisterAll()
+  console.log("Reloading...");
+  config = JSON.parse(cfg);
+  globalShortcut.unregisterAll();
   try {
-    globalShortcut.register(config.shortcut, takeScreenshot)
-  } catch(e) {
-    console.log(e)
-    globalShortcut.register("CommandOrControl+Shift+C", takeScreenshot)
+    globalShortcut.register(config.shortcut, takeScreenshot);
+  } catch (e) {
+    console.log(e);
+    globalShortcut.register("CommandOrControl+Shift+C", takeScreenshot);
   }
-})
+});
 
-let win = null
+let win = null;
 async function showSettingsWindow() {
   // if(win) win.close()
-  win = new BrowserWindow({width: 500, height: 500})
-  win.show()
-  win.focus()
-  win.loadURL("file://" + __dirname + "/web/settings.html")
+  win = new BrowserWindow({ width: 500, height: 500 });
+  win.show();
+  win.focus();
+  win.loadURL("file://" + __dirname + "/web/settings.html");
 }
 
 async function showAccountWindow() {
   // if(win) win.close()
-  win = new BrowserWindow({width: 800, height: 600})
-  win.show()
-  win.focus()
-  win.loadURL("file://" + __dirname + "/web/account.html")
+  win = new BrowserWindow({ width: 800, height: 600 });
+  win.show();
+  win.focus();
+  win.loadURL("file://" + __dirname + "/web/account.html");
 }
 
-let tray = null
+let tray = null;
 app.on("ready", function() {
-  tray = new Tray(__dirname + "/resources/iconTemplate.png")
+  tray = new Tray(__dirname + "/resources/iconTemplate.png");
   const contextMenu = Menu.buildFromTemplate([
-    {label: "Take screenshot", click: takeScreenshot},
-    {label: "My account", click: showAccountWindow},
-    {label: "Settings", click: showSettingsWindow},
-    {type: "separator"},
-    {label: "Quit", role: "quit"}
-  ])
-  const keyboardShortcut = os.platform === "darwin" ? "⇧⌘C" : "Ctrl-Shift-C"
-  tray.setToolTip(`Press ${keyboardShortcut} to take a screenshot`)
-  tray.setContextMenu(contextMenu)
+    { label: "Take screenshot", click: takeScreenshot },
+    { label: "My account", click: showAccountWindow },
+    { label: "Settings", click: showSettingsWindow },
+    { type: "separator" },
+    { label: "Quit", role: "quit" }
+  ]);
+  const keyboardShortcut = os.platform === "darwin" ? "⇧⌘C" : "Ctrl-Shift-C";
+  tray.setToolTip(`Press ${keyboardShortcut} to take a screenshot`);
+  tray.setContextMenu(contextMenu);
   try {
-    globalShortcut.register(config.shortcut, takeScreenshot)
-  } catch(e) {
-    console.log(e)
-    globalShortcut.register("CommandOrControl+Shift+C", takeScreenshot)
+    globalShortcut.register(config.shortcut, takeScreenshot);
+  } catch (e) {
+    console.log(e);
+    globalShortcut.register("CommandOrControl+Shift+C", takeScreenshot);
   }
-})
+});
 
 app.on("will-quit", function() {
-  globalShortcut.unregisterAll()
-})
+  globalShortcut.unregisterAll();
+});
 
 const hostMap = {
   pomf: pomfUpload,
-  tig: tigUpload,
+  kuvien: kuvienUpload,
   srht: srhtUpload,
   owo: owoUpload,
   nothingdomains: nothingDomainsUpload,
   elixire: elixireUpload
-}
+};
 
 async function takeScreenshot() {
   try {
-    let buffer = await capture()
-    if(!buffer) return false // The user canceled the screenshot
-    let url = null
-    url = await hostMap[config.img_host](buffer)
-    clipboard.writeText(url)
-    console.log(`Uploaded as ${url}`)
+    let buffer = await capture();
+    console.log("Captured!");
+    if (!buffer) return false; // The user canceled the screenshot
+    let url = null;
+    url = await hostMap[config.img_host](buffer);
+    clipboard.writeText(url);
+    console.log(`Uploaded as ${url}`);
     new Notification({
       title: "TIGCap",
       body: "Link to screenshot copied to clipboard!",
       silent: true
-    }).show()
-  } catch(e) {
+    }).show();
+  } catch (e) {
     // Ignore errors about not running two at once
-    if(e.includes && e.includes("screencapture: cannot run two interactive screen captures at a time")) return
-    throw e
+    if (
+      e.includes &&
+      e.includes(
+        "screencapture: cannot run two interactive screen captures at a time"
+      )
+    )
+      return;
+    throw e;
   }
 }
 
 async function srhtUpload(buffer) {
-  let res = await snekfetch.get(`${config.srht_url}/api/upload`).attach("file", buffer, "oof.png").send({key: config.srht_key})
-  return res.body.url
+  let res = await snekfetch
+    .get(`${config.srht_url}/api/upload`)
+    .attach("file", buffer, "oof.png")
+    .send({ key: config.srht_key });
+  return res.body.url;
 }
 
 async function nothingDomainsUpload(buffer) {
-  let vanity = config.nothingdomains_vanity
-  let res
+  let vanity = config.nothingdomains_vanity;
+  let res;
   try {
-    res = await snekfetch.post("https://nothing.domains/api/upload/pomf").attach("files[]", buffer, "oof.png").set({
-      Authorization: config.nothingdomains_key
-    })
+    res = await snekfetch
+      .post("https://nothing.domains/api/upload/pomf")
+      .attach("files[]", buffer, "oof.png")
+      .set({
+        Authorization: config.nothingdomains_key
+      });
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
-  let body = res.body
-  if(res.body instanceof Buffer) {
-    body = JSON.parse(res.body.toString("utf8"))
+  let body = res.body;
+  if (res.body instanceof Buffer) {
+    body = JSON.parse(res.body.toString("utf8"));
   }
-  if(!body.success) throw "Unknown error"
-  return `${vanity}${body.files[0].url}`
+  if (!body.success) throw "Unknown error";
+  return `${vanity}${body.files[0].url}`;
 }
 
 async function elixireUpload(buffer) {
-  let res
+  let res;
   try {
-    res = await snekfetch.post("https://elixi.re/api/upload").attach("f[]", buffer, "oof.png").set({
-      Authorization: config.elixire_key
-    })
+    res = await snekfetch
+      .post("https://elixi.re/api/upload")
+      .attach("f[]", buffer, "oof.png")
+      .set({
+        Authorization: config.elixire_key
+      });
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
-  let body = res.body
-  if(res.body instanceof Buffer) {
-    body = JSON.parse(res.body.toString("utf8"))
+  let body = res.body;
+  if (res.body instanceof Buffer) {
+    body = JSON.parse(res.body.toString("utf8"));
   }
-  return `${body.url}`
+  return `${body.url}`;
 }
 
 async function pomfUpload(buffer) {
-  let picked = config.pomf_host
+  let picked = config.pomf_host;
   // let picked
-  if(!picked) {
-    let res1 = await snekfetch.get("https://rawgit.com/lc-guy/limf/master/host_list.json")
-    let list = res1.body
-    picked = list[Math.floor(Math.random() * list.length)]
+  if (!picked) {
+    let res1 = await snekfetch.get(
+      "https://rawgit.com/lc-guy/limf/master/host_list.json"
+    );
+    let list = res1.body;
+    picked = list[Math.floor(Math.random() * list.length)];
   }
-  let res
+  let res;
   try {
-    res = await snekfetch.post(`${picked[0]}upload.php`).attach("files[]", buffer, "oof.png")
+    res = await snekfetch
+      .post(`${picked[0]}upload.php`)
+      .attach("files[]", buffer, "oof.png");
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
-  let body = res.body
-  if(res.body instanceof Buffer) {
-    body = JSON.parse(res.body.toString("utf8"))
+  let body = res.body;
+  if (res.body instanceof Buffer) {
+    body = JSON.parse(res.body.toString("utf8"));
   }
-  if(!body.success) throw "Unknown error"
-  if(!body.files[0].url.match(new RegExp(picked[1], "gi"))) return picked[1] + body.files[0].url
-  else return body.files[0].url
+  if (!body.success) throw "Unknown error";
+  if (!body.files[0].url.match(new RegExp(picked[1], "gi")))
+    return picked[1] + body.files[0].url;
+  else return body.files[0].url;
 }
 
 async function owoUpload(buffer) {
-  let res = await snekfetch.post("https://api.awau.moe/upload/pomf").attach("files[]", buffer, "oof.png").set({authorization: config.owoToken})
-  let url = `${config.owoUrl || "https://owo.whats-th.is/"}${res.body.files[0].url}`
-  return url
+  let res = await snekfetch
+    .post("https://api.awau.moe/upload/pomf")
+    .attach("files[]", buffer, "oof.png")
+    .set({ authorization: config.owoToken });
+  let url = `${config.owoUrl || "https://owo.whats-th.is/"}${
+    res.body.files[0].url
+  }`;
+  return url;
 }
 
-async function tigUpload(buffer) {
-  let res = await snekfetch.post("https://theimg.guru/ajaxupload").attach("file", buffer, "oof.png")
-  return res.body.name
+async function kuvienUpload(buffer) {
+  let res = await snekfetch
+    .post("https://api.kuvien.io/image/upload")
+    .set("X-App-Key", config.kuvien_key)
+    .attach("file", buffer, "oof.png");
+  return res.body.file.url;
 }
 
 app.on("window-all-closed", function() {
   // Ignore it
-})
+});
--- web/settings.js
+++ web/settings.js
@@ -1,109 +1,123 @@
-var {ipcRenderer, remote} = require("electron")
+var { ipcRenderer, remote } = require("electron");
 
-var config
+var config;
 
 try {
-  config = require(getAppDir() + "/config.json")
-} catch(e) {
+  config = require(getAppDir() + "/config.json");
+} catch (e) {
   config = {
-    "img_host": "tig",
-    "srht_url": "https://srht.example.org",
-    "srht_key": "Example",
-    "pomf_host": ["https://pomf.example.org/", "https://pomf-vanity.example.org/"],
-    "owoToken": "",
-    "owoUrl": "https://owo.whats-th.is/",
-    "shortcut": "CommandOrControl+Shift+C"
-  }
+    img_host: "elixire",
+    srht_url: "https://srht.example.org",
+    srht_key: "Example",
+    pomf_host: [
+      "https://pomf.example.org/",
+      "https://pomf-vanity.example.org/"
+    ],
+    owoToken: "",
+    owoUrl: "https://owo.whats-th.is/",
+    shortcut: "CommandOrControl+Shift+C",
+    kuvien_key: ""
+  };
 }
 
-var fs = require("fs")
+var fs = require("fs");
 window.addEventListener("load", function() {
-  var imgHostSelector = document.getElementById("img-host")
+  var imgHostSelector = document.getElementById("img-host");
   imgHostSelector.addEventListener("change", function() {
-    config.img_host = this.value
-    reloadValues()
-  })
-  var body = document.getElementById("body")
-  var srht_url = document.getElementById("srht_url")
-  var srht_key = document.getElementById("srht_key")
-  var pomf_host_server = document.getElementById("pomf_host_server")
-  var pomf_host_vanity = document.getElementById("pomf_host_vanity")
-  var owo_token = document.getElementById("owo_token")
-  var owo_url = document.getElementById("owo_url")
-  var shortcut_text = document.getElementById("shortcut_text")
-  var nothingdomains_vanity = document.getElementById("nothingdomains_vanity")
-  var nothingdomains_key = document.getElementById("nothingdomains_key")
-  var elixire_key = document.getElementById("elixire_key")
-
+    config.img_host = this.value;
+    reloadValues();
+  });
+  var body = document.getElementById("body");
+  var srht_url = document.getElementById("srht_url");
+  var srht_key = document.getElementById("srht_key");
+  var pomf_host_server = document.getElementById("pomf_host_server");
+  var pomf_host_vanity = document.getElementById("pomf_host_vanity");
+  var owo_token = document.getElementById("owo_token");
+  var owo_url = document.getElementById("owo_url");
+  var shortcut_text = document.getElementById("shortcut_text");
+  var nothingdomains_vanity = document.getElementById("nothingdomains_vanity");
+  var nothingdomains_key = document.getElementById("nothingdomains_key");
+  var elixire_key = document.getElementById("elixire_key");
+  var kuvien_key = document.getElementById("kuvien_key");
 
   nothingdomains_key.addEventListener("change", function() {
-    config.nothingdomains_key = this.value
-    save()
-  })
+    config.nothingdomains_key = this.value;
+    save();
+  });
   nothingdomains_vanity.addEventListener("change", function() {
-    config.nothingdomains_vanity = this.value
-    save()
-  })
+    config.nothingdomains_vanity = this.value;
+    save();
+  });
   srht_url.addEventListener("change", function() {
-    config.srht_url = this.value
-    save()
-  })
+    config.srht_url = this.value;
+    save();
+  });
   srht_key.addEventListener("change", function() {
-    config.srht_key = this.value
-    save()
-  })
+    config.srht_key = this.value;
+    save();
+  });
   pomf_host_server.addEventListener("change", function() {
-    config.pomf_host[0] = this.value
-    save()
-  })
+    config.pomf_host[0] = this.value;
+    save();
+  });
   pomf_host_vanity.addEventListener("change", function() {
-    config.pomf_host[1] = this.value
-    save()
-  })
+    config.pomf_host[1] = this.value;
+    save();
+  });
   owo_token.addEventListener("change", function() {
-    config.owoToken = this.value
-    save()
-  })
+    config.owoToken = this.value;
+    save();
+  });
   owo_url.addEventListener("change", function() {
-    config.owoUrl = this.value
-    save()
-  })
+    config.owoUrl = this.value;
+    save();
+  });
+  kuvien_key.addEventListener("change", function() {
+    config.kuvien_key = this.value;
+    save();
+  });
   elixire_key.addEventListener("change", function() {
-    config.elixire_key = this.value
-    save()
-  })
+    config.elixire_key = this.value;
+    save();
+  });
   shortcut_text.addEventListener("change", function() {
-    config.shortcut = this.value
-    save()
-  })
+    config.shortcut = this.value;
+    save();
+  });
 
   function reloadValues() {
-    srht_url.value = config.srht_url
-    srht_key.value = config.srht_key
-    pomf_host_server.value = config.pomf_host[0]
-    pomf_host_vanity.value = config.pomf_host[1]
-    owo_token.value = config.owoToken
-    owo_url.value = config.owoUrl
-    body.classList = config.img_host
-    imgHostSelector.value = config.img_host
-    shortcut_text.value = config.shortcut || "CommandOrControl+Shift+C"
-    nothingdomains_key.value = config.nothingdomains_key
-    nothingdomains_vanity.value = config.nothingdomains_vanity
-    elixire_key.value = config.elixire_key
-    save()
+    srht_url.value = config.srht_url;
+    srht_key.value = config.srht_key;
+    pomf_host_server.value = config.pomf_host[0];
+    pomf_host_vanity.value = config.pomf_host[1];
+    owo_token.value = config.owoToken;
+    owo_url.value = config.owoUrl;
+    body.classList = config.img_host;
+    imgHostSelector.value = config.img_host;
+    shortcut_text.value = config.shortcut || "CommandOrControl+Shift+C";
+    nothingdomains_key.value = config.nothingdomains_key;
+    nothingdomains_vanity.value = config.nothingdomains_vanity;
+    elixire_key.value = config.elixire_key;
+    kuvien_key.value = config.kuvien_key;
+    save();
   }
-  window.reloadValues = reloadValues
-  reloadValues()
-})
+  window.reloadValues = reloadValues;
+  reloadValues();
+});
 function save() {
-  fs.writeFile(getAppDir() + "/config.json", JSON.stringify(config), {flag: "w"}, function(err, oof) {
-    console.log(err, oof)
-    if(err) throw err
-    console.log("Saved!")
-    ipcRenderer.send("reload", JSON.stringify(config))
-  })
+  fs.writeFile(
+    getAppDir() + "/config.json",
+    JSON.stringify(config),
+    { flag: "w" },
+    function(err, oof) {
+      console.log(err, oof);
+      if (err) throw err;
+      console.log("Saved!");
+      ipcRenderer.send("reload", JSON.stringify(config));
+    }
+  );
 }
 
 function getAppDir() {
-  return remote.app.getPath("userData")
+  return remote.app.getPath("userData");
 }
